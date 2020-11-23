e5856fb0c4f2c4363bc7f4ecf075c6cc295625da
unknown <jreidmke@gmail.com>
2020-11-23 13:27:11 -0600

    updated add card functions

--- src/Deck.js
+++ src/Deck.js
@@ -9,36 +9,30 @@ const Deck = () => {
     const [deck, setDeck] = useState([]);
     async function addRandom() {
         const rand = Math.ceil(Math.random() * 82);
-        const resp = await axios.get(`https://swapi.dev/api/people/${rand}`);
-        setDeck(c => [
-            ...c,
-            {
-                key: uuidv4(),
-                name: resp.data.name,
-                height: resp.data.height,
-                mass: resp.data.mass,
-                birth_year: resp.data.birth_year
-            }
-        ]);
+        const resp = (await axios.get(`https://swapi.dev/api/people/${rand}`)).data;
+        setData(resp);
     }
 
     async function addSearch(term) {
         try {
             const resp = (await axios.get(`https://swapi.dev/api/people/?search=${term}`)).data.results[0];
-            console.log(resp);
-            setDeck(c => [
-                ...c,
-                {
-                    key: uuidv4(),
-                    name: resp.name,
-                    height: resp.height,
-                    mass: resp.mass,
-                    birth_year: resp.birth_year
-                }
-            ]);
+            setData(resp);
         } catch (error) {
             console.log(error);
         }
+    }
+
+    function setData(resp) {
+        setDeck(c => [
+            ...c,
+            {
+                key: uuidv4(),
+                name: resp.name,
+                height: resp.height,
+                mass: resp.mass,
+                birth_year: resp.birth_year
+            }
+        ]);
     }
 
     const cards = deck.map(c => <Card key={c.key} name={c.name} height={c.height} mass={c.mass} birth_year={c.birth_year}/>)
