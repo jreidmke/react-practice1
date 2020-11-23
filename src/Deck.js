9295087b72d188f810c67313addaefc98b811df6
unknown <jreidmke@gmail.com>
2020-11-23 10:36:23 -0600

    completed deck version 1

--- src/Deck.js
+++ src/Deck.js
@@ -4,19 +4,14 @@ import Card from './Card';
 const { v4: uuidv4 } = require('uuid');
 
 const Deck = () => {
-    //start with an empty deck in state
     const [deck, setDeck] = useState([]);
-
-    //create an add card function
-
-    //ok wait. hold on. before we start doin that, lets make a rndom interger to use.
-
     async function add() {
         const rand = Math.ceil(Math.random() * 82);
         const resp = await axios.get(`https://swapi.dev/api/people/${rand}`);
         setDeck(c => [
             ...c,
             {
+                key: uuidv4(),
                 name: resp.data.name,
                 height: resp.data.height,
                 mass: resp.data.mass,
@@ -25,7 +20,7 @@ const Deck = () => {
         ]);
     }
 
-    const cards = deck.map(c => <Card key={uuidv4()} name={c.name} height={c.height} mass={c.mass} birth_year={c.birth_year}/>)
+    const cards = deck.map(c => <Card key={c.key} name={c.name} height={c.height} mass={c.mass} birth_year={c.birth_year}/>)
 
     return(
         <div>
