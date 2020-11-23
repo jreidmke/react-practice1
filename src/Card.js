261743e3b6dbbe4d43989f92dee51ffec4e142b3
unknown <jreidmke@gmail.com>
2020-11-23 10:06:30 -0600

    completed card

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+const Card = ({name, height, mass, birth_year}) => {
+    return(
+    <div>
+        <h1>{name}</h1>
+        <h4>Height: {height}</h4>
+        <h4>Mass: {mass}</h4>
+        <h4>Birth Year: {birth_year}</h4>
+    </div>)
+}
+
+export default Card;
